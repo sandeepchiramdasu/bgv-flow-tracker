72db2b042563fd650433f96e7462ee873af0ffce
chiramdasusandeep <chiramdasusandeep@gmail.com>
2026-05-17 09:51:59 +0530

    custom user model

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role Information", {
+            "fields": ("role",)
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            "fields": ("role",),
+        }),
+    )
+
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "is_staff",
+        "is_active",
+    )
